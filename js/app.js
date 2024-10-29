89c480351b670f46291633fa3eba63ea62403143
Diego Alejandro Ortiz G <ortiz.11.diego@gmail.com>
2024-10-28 21:40:55 -0500

    second part of menu

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 let menu = document.querySelector(".hamburguesa");
 let navegacion = document.querySelector(".navegacion");
 let imagenes = document.querySelectorAll("img");
+let btnTodos = document.querySelector(".todos");
+let btnEnsaladas = document.querySelector(".ensaladas");
+let btnPasta = document.querySelector(".pasta");
+let btnPizza = document.querySelector(".pizza");
+let btnPostres = document.querySelector(".postres");
+let contenedorPlatillos = document.querySelector(".platillos");
 
 document.addEventListener("DOMContentLoaded", () => {
   eventos();
+  platillos();
 });
 
 const eventos = () => menu.addEventListener('click', abrirMenu);
@@ -26,9 +33,20 @@ const buttonClose = () => {
   closeMenu(btnClose, overlay);
 }
 
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      const imagen = entry.target;
+      imagen.src = imagen.dataset.src;
+      observer.unobserve(imagen);
+    }
+  });
+})
+
 imagenes.forEach(imagen => {
-  imagen.src = imagen.dataset.src;
+  observer.observe(imagen);
 });
+
 
 const closeMenu = (button, overlay) => {
   button.addEventListener('click', () => {
@@ -43,3 +61,44 @@ const closeMenu = (button, overlay) => {
     boton.remove();
   }
 }
+
+const platillos = () => {
+  let platillosArreglo = [];
+  const platillos = document.querySelectorAll(".platillo");
+  platillos.forEach(platillo => platillosArreglo = [...platillosArreglo, platillo]);
+  const ensaladas = platillosArreglo.filter(ensalada=>ensalada.getAttribute("data-platillo") === "ensalada");
+  const pastas = platillosArreglo.filter(pastas=>pastas.getAttribute("data-platillo") === "pasta");
+  const pizzas = platillosArreglo.filter(pizza=>pizza.getAttribute("data-platillo") === "pizza");
+  const postres = platillosArreglo.filter(postres=>postres.getAttribute("data-platillo") === "postre");
+  mostrarPlatillos(ensaladas, pastas, pizzas, postres, platillosArreglo);
+}
+
+const mostrarPlatillos = (ensaladas, pastas, pizzas, postres, todos) => {
+  btnEnsaladas.addEventListener('click', () => {
+    limpiarHtml(contenedorPlatillos);
+    ensaladas.forEach(ensalada => contenedorPlatillos.appendChild(ensalada));
+  });
+  btnPasta.addEventListener('click', () => {
+    limpiarHtml(contenedorPlatillos);
+    pastas.forEach(pasta => contenedorPlatillos.appendChild(pasta));
+  });
+  btnPizza.addEventListener('click', () => {
+    limpiarHtml(contenedorPlatillos);
+    pizzas.forEach(pizza => contenedorPlatillos.appendChild(pizza));
+  });
+  btnPostres.addEventListener('click', () => {
+    limpiarHtml(contenedorPlatillos);
+    postres.forEach(postre => contenedorPlatillos.appendChild(postre));
+  });
+  btnTodos.addEventListener('click', () => {
+    limpiarHtml(contenedorPlatillos);
+    todos.forEach(todo => contenedorPlatillos.appendChild(todo));
+  });
+}
+
+const limpiarHtml = (contenedor) => {
+  while(contenedor.firstChild){
+    contenedor.removeChild(contenedor.firstChild);
+  }
+}
+
